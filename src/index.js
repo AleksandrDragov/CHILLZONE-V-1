1797635b1a3c324ab46f781698dd48db2ef0b2cb
AleksandrDragov <118523521+AleksandrDragov@users.noreply.github.com>
2024-04-16 17:06:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/CHILLZONE-V-1">
       <App />
     </BrowserRouter>
   </React.StrictMode>

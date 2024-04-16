eaefa25affcd4c0b61a250ccfdf293e5a67bf70c
AleksandrDragov <118523521+AleksandrDragov@users.noreply.github.com>
2024-04-16 15:01:03 +0000

    Deploying to gh-pages from @ AleksandrDragov/CHILLZONE-V-1@fcbbaba01286e3772dd05e17def13aa9610f268f 🚀

---
+++ static/js/80.cb2effb4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[80],{80:(e,t,r)=>{r.r(t),r.d(t,{default:()=>c});r(791);var a=r(184);const c=()=>(0,a.jsx)("div",{children:(0,a.jsx)("h1",{style:{color:"white"},children:"Home Page"})})}}]);
+//# sourceMappingURL=80.cb2effb4.chunk.js.map
